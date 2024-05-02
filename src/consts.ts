b1351e75dfecc7741d9542c0884f9c6054e736b2
rosariopf <rosariopf@google.com>
2024-05-02 03:45:29 +0100

    🎨 Create initial portfolio

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'thatfire.dev';
+export const SITE_AUTHOR = 'Rosário P. Fernandes'
+export const PORTFOLIO_TITLE = SITE_AUTHOR + ' | Portfolio';
+export const BLOG_TITLE = SITE_AUTHOR + ' | Blog';
+export const SITE_DESCRIPTION = 'DevRel Engineer at Firebase';
